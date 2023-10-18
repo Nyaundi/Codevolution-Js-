ec90fa4ce9e3ab23e418a2e3003aefde17951ef0
Nyaundi <jamesmabuka260@gmail.com>
2023-10-18 10:51:25 +0300

    added js

---
+++ script.js
@@ -0,0 +1,14 @@
+//Object
+
+const person = {
+    'fistName': 'Bruce',
+    'lastName': 'Wayne',
+    'age': 30
+}
+console.log(person.fistName);
+
+//Arrays
+
+const oddNumbers = [1, 3,5, 7, 9]
+
+console.log(oddNumbers[2]);
