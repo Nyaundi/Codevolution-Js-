861b7f663c54c1604c19fb980e54d3e18b30d540
Nyaundi <jamesmabuka260@gmail.com>
2023-10-18 11:31:02 +0300

    added refactoring

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-//Object
+// //Object
 
-const person = {
-    'fistName': 'Bruce',
-    'lastName': 'Wayne',
-    'age': 30
-}
-console.log(person.fistName);
+// const person = {
+//     'fistName': 'Bruce',
+//     'lastName': 'Wayne',
+//     'age': 30
+// }
+// console.log(person.fistName);
 
-//Arrays
+// //Arrays
 
-const oddNumbers = [1, 3,5, 7, 9]
+// const oddNumbers = [1, 3,5, 7, 9]
 
-console.log(oddNumbers[2]);
+// console.log(oddNumbers[2]);
+
+let x = 10 //assignment operator
+let y = 5; 
+
+console.log(x + y);
+
+console.log(x > y);
+
+const isValidNum = x > 8 && 8> y;
+console.log(isValidNum);
