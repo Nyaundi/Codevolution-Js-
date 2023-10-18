5ba3c347d23911c66b48293fd8bbdbbf923f40eb
Nyaundi <jamesmabuka260@gmail.com>
2023-10-18 11:47:48 +0300

    conditionals

--- script.js
+++ script.js
@@ -13,12 +13,33 @@
 
 // console.log(oddNumbers[2]);
 
-let x = 10 //assignment operator
-let y = 5; 
+// let x = 10 //assignment operator
+// let y = 5; 
 
-console.log(x + y);
+// console.log(x + y);
 
-console.log(x > y);
+// console.log(x > y);
 
-const isValidNum = x > 8 && 8> y;
-console.log(isValidNum);
+// const isValidNum = x > 8 && 8> y;
+// console.log(isValidNum);
+
+
+// console.log('bruce' + ' Wayne');
+
+// //Ternary operator
+
+// const isEven = 10 % 2 === 0 ? true : false;
+// console.log(isEven);
+
+//Implicit or type coersion conversion ==> javascript will do the convertion automatically
+// explicit conversion ==> you manually convert the type
+
+//Implicit or type coersion conversion
+
+// console.log('4' % '2');
+
+// Equality
+// ==Allows coercion 
+// === Does not allow coercion
+
+//Conditional statements
